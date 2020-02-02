182a8d0a42273b66b8256d73ef9cc7e0b714051e
Bruno Murino <bfsmurino@gmail.com>
2020-02-02 16:21:23 +0000

    successfully read data from database using standalone spark cluster

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # Project Spark
 
 Python dependencies must also be installed on the spark cluster
+
+Run `docker-compose up` to run everything.
+
+Since the containers are named we can easily ssh into them by doing e.g.
+
+```
+docker exec -ti mysql_db bash
+```
+
+* To see the python script working, create a table on the database
--- streaming/spark-basic.py
+++ streaming/spark-basic.py
@@ -1,20 +1,27 @@
 # spark-basic.py
 import os
-from pyspark import SparkConf
-from pyspark import SparkContext
-import numpy as np
+from pyspark import SparkConf, SparkContext, SQLContext
+
 os.environ["PYSPARK_PYTHON"] = "/usr/bin/python3"
 os.environ["PYSPARK_DRIVER_PYTHON"] = "/usr/bin/python3"
 
 conf = SparkConf()
 conf.setMaster('spark://spark-master:7077')
 conf.setAppName('spark-basic')
+conf.set("spark.jars", "/usr/share/java/mariadb-java-client-2.5.4.jar")
 sc = SparkContext(conf=conf)
 
+sqlContext = SQLContext(sc)
 
-def mod(x):
-    return (x, np.mod(x, 2))
+df = sqlContext.read \
+    .format("jdbc") \
+    .option("url", "jdbc:mysql://mysql:3306") \
+    .option("driver", "org.mariadb.jdbc.Driver") \
+    .option("dbtable", "testing_db.bm") \
+    .option("user", "bruno") \
+    .option("password", "password") \
+    .load()
 
+df.show()
 
-rdd = sc.parallelize(range(1000)).map(mod).take(10)
-print(rdd)
+sc.stop()
